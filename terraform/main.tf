6c4a88bcfb64dccf8db1d9af0a1027433764aa6e
Afraz Ahmed <aphraz@live.com>
2023-09-05 03:43:27 +0500

    Ingress controller with TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,20 @@ module "eks" {
     }
   }
 }
+
+module "nginx-controller" {
+  source  = "terraform-iaac/nginx-controller/helm"
+
+  additional_set = [
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+      type  = "string"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+      value = "true"
+      type  = "string"
+    }
+  ]
+}
