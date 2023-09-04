add6b2e38318bdf2f235546c3103dd06477da70b
Afraz Ahmed <aphraz@live.com>
2023-09-05 04:42:43 +0500

    Ingress controller with TF

--- terraform/ingress-nginx.tf
+++ terraform/ingress-nginx.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = var.ingress_nginx_helm_version
+  
 
   namespace        = var.ingress_nginx_namespace
   create_namespace = true
