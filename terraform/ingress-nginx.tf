5b00c3e062d8058d73b1ef1e4697115a14742f0d
Afraz Ahmed <aphraz@live.com>
2023-09-05 04:21:58 +0500

    Ingress controller with TF

---
+++ terraform/ingress-nginx.tf
@@ -0,0 +1,31 @@
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = var.ingress_nginx_helm_version
+
+  namespace        = var.ingress_nginx_namespace
+  create_namespace = true
+
+ 
+
+  depends_on = [local.cluster_name]
+}
+
+resource "null_resource" "wait_for_ingress_nginx" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the nginx ingress controller...\n"
+      kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=controller \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.ingress_nginx]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -65,20 +65,3 @@ module "eks" {
     }
   }
 }
-
-module "nginx-controller" {
-  source  = "terraform-iaac/nginx-controller/helm"
-
-  additional_set = [
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "nlb"
-      type  = "string"
-    },
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-      value = "true"
-      type  = "string"
-    }
-  ]
-}
