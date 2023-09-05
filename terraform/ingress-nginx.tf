b3369106ec112996e08d533238b44803ab20118d
Afraz Ahmed <aphraz@live.com>
2023-09-05 22:34:14 +0500

    Added Kubernetes provider

--- terraform/ingress-nginx.tf
+++ terraform/ingress-nginx.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = module.eks.cluster_iam_role_arn
-  load_config_file       = false
+  
 }
 
 provider "helm" {
