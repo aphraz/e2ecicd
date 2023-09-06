30df19233205c3693b07a369de64220ede999dc9
Afraz Ahmed <aphraz@live.com>
2023-09-06 06:32:10 +0500

    multiple changes

--- terraform/ingress-nginx.tf
+++ terraform/ingress-nginx.tf
@@ -13,7 +13,7 @@ resource "null_resource" "kubectl" {
   provisioner "local-exec" {
     command = "aws eks --region ${var.region} update-kubeconfig --name ${local.cluster_name}"
   }
-  depends_on = [module.eks]
+  depends_on = [module.eks.cluster_name]
 }
 
 resource "helm_release" "ingress_nginx" {
